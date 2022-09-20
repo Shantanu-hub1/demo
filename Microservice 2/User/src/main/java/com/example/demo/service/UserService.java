f81f6ed8181c11abd3f26b6f84a253bc9ae7115c
Shantanu <shantanu.khamrui@nitsolution.in>
2022-09-20 12:22:33 +0530

    Shantanu

--- Microservice 2/User/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.User;
-
-public interface UserService {
-	
-	public User getuser(long userid);
-
-}
--- MongoDb/src/main/java/com/Ulearn/PackageService/exception/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.Ulearn.PackageService.exception;
-
-public class CustomException extends RuntimeException{
-	
-	 public CustomException(String message) {
-	        super(message);
-	    }
-
-}
